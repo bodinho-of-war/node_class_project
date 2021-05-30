6f8a06bf17843efb018b0898bf073e09ed252915
Luan Portela <luan@t2tsolucoes.com>
2021-05-29 23:25:18 -0300

    login method

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const http = require('http')
-const url = require('url')
+const querystring = require('querystring')
 const PORT = 3000
 
 const server = http.createServer((req, res) => {
-    console.log(req.headers)
     const parsedUrl = new URL(req.url, `http://${req.headers.host}`)
-    
     const { pathname } = parsedUrl
     const trimmedPath = pathname.replace(/^\/+|\/+$/g, '')
-    
+    const method = req.method.toLowerCase()
+
+    const queryStringObject = parsedUrl.searchParams
+
     res.end("Hello World!\n")
 
-    console.log(`Request is received on this path: ${trimmedPath}`)
+    console.log(`Request is received on this path: ${trimmedPath} whit this method: ${method} and with these query string parameters: ${queryStringObject}`)
 })
 
 server.listen(PORT,() => console.log(`The server is listening on port ${PORT}`))
