acb39d664500d614277f929520abc8102b87f152
Luan Portela <luan@t2tsolucoes.com>
2021-05-29 23:03:22 -0300

    log request path

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const http = require('http')
+const url = require('url')
 const PORT = 3000
 
-const server = http.createServer((req, res) => res.end("Hello World!\n"))
+const server = http.createServer((req, res) => {
+    console.log(req.headers)
+    const parsedUrl = new URL(req.url, `http://${req.headers.host}`)
+    
+    const { pathname } = parsedUrl
+    const trimmedPath = pathname.replace(/^\/+|\/+$/g, '')
+    
+    res.end("Hello World!\n")
+
+    console.log(`Request is received on this path: ${trimmedPath}`)
+})
 
 server.listen(PORT,() => console.log(`The server is listening on port ${PORT}`))
 
